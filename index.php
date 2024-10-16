8ae600ed5c455f4514b7201234c94443578200d2
Адлан Шамавов <a.shamavov@iqdev.digital>
2024-10-17 00:22:04 +0500

    Добавлены классы

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+abstract class Product
+{
+    private float $price;
+
+    public abstract function countPrice();
+}
+
+class RetailProduct extends Product
+{
+    private int $count;
+
+    public function countPrice(): float
+    {
+        return $this->price * $this->count;
+    }
+}
+
+class DigitalProduct extends Product
+{
+    public function countPrice(): float
+    {
+        return $this->price / 2;
+    }
+}
+
+class WeightProduct extends Product
+{
+    private float $weight;
+
+    public function countPrice(): float
+    {
+        return $this->price * $this->weight / 1000;
+    }
+}
